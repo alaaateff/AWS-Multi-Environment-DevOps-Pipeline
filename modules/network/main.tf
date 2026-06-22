d4347c25b4548bf38f3eee262471366c017b2d3e
alaaateff <alaaatef3200@gmail.com>
2026-06-23 00:12:04 +0300

    added nat gateway

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -36,6 +36,14 @@ resource "aws_route" "public_route" {
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main.id
+
+}
+
+resource "aws_route" "private_route" {
+  route_table_id            = aws_route_table.private_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+
 }
 
 resource "aws_route_table_association" "public" {
@@ -53,4 +61,17 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
+resource "aws_eip" "eip_nat" {
+  domain   = "vpc"
+}
 
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip_nat.id
+  subnet_id     = aws_subnet.public["a"].id
+
+  tags = {
+    Name = "Nat gateway"
+  }
+
+  depends_on = [aws_internet_gateway.gw]
+}
