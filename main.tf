7bcb848be5b0912e29c86397a9349baae8542213
alaaateff <alaaatef3200@gmail.com>
2026-06-23 00:41:06 +0300

    modified referencing issues

--- main.tf
+++ main.tf
@@ -41,13 +41,6 @@ resource "aws_security_group" "second_sg" {
     protocol    = "tcp"
     cidr_blocks = [module.network.vpc_cidr]
   }
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    source_security_group_id = aws_security_group.alb_sg.id
-  }
-
   egress {
   from_port   = 0
   to_port     = 0
@@ -265,12 +258,22 @@ resource "aws_security_group" "alb_sg" {
 
 }
 
+resource "aws_security_group_rule" "alb_to_app_3000" {
+  type                     = "ingress"
+  from_port                = 3000
+  to_port                  = 3000
+  protocol                 = "tcp"
+
+  security_group_id        = aws_security_group.second_sg.id
+  source_security_group_id = aws_security_group.alb_sg.id
+}
+
 resource "aws_lb" "alb" {
   name               = "alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  subnets            = [module.network.public_subnet_ids]
 
   tags = {
     Environment = "${terraform.workspace}"
@@ -281,7 +284,7 @@ resource "aws_lb_target_group" "alb_target_group" {
   name     = "alb_target_group"
   port     = 3000
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = module.network.vpc_id
 }
 
 resource "aws_lb_listener" "alb_listener" {
